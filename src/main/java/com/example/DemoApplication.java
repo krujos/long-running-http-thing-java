2183987e003988711c60915c82ba5aa7a1cedfe8
krujos <jkruck@pivotal.io>
2015-10-15 12:27:04 -0500

    Impl

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,14 +2,40 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-    @RequestMapping(value="/")
-    public String root() {
-        return "Hello"
+    private static int twoMin = 1000 * 120;
+
+    @RequestMapping(value="/sync")
+    public String sync() throws InterruptedException {
+        Thread.sleep(twoMin);
+        return "Hello!";
+    }
+
+    @RequestMapping(value="defered")
+    public DeferredResult<String> defered() {
+        final DeferredResult<String> result = new DeferredResult<>((long) (twoMin + 1000));
+        new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(twoMin);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                result.setResult("Hello from another thread!");
+            }
+        }.start();
+
+        System.out.println("Started");
+        return result;
+
+
     }
 
     public static void main(String[] args) {
